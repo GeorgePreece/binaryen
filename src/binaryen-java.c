669e4e6d78d8a521ea91a67738aa3f0bee0056d6
George Preece <georgejouzuna@gmail.com>
2021-12-23 19:13:39 +0000

    Create test function

--- src/binaryen-java.c
+++ src/binaryen-java.c
@@ -1,3 +1,10 @@
 #include "binaryen-java.h"
 
-//
+JNIEXPORT jobject JNICALL 
+Java_WebAssembly_test(
+    JNIEnv *env, 
+    jclass this
+) {
+    puts("Hello, World!");
+    return (void *)0;
+}
--- src/binaryen-java.h
+++ src/binaryen-java.h
@@ -3,4 +3,10 @@
 #ifndef wasm_binaryen_java_h
 #define wasm_binaryen_java_h
 
+JNIEXPORT jobject JNICALL 
+Java_WebAssembly_test(
+    JNIEnv *, 
+    jclass
+);
+
 #endif // wasm_binaryen_java_h
